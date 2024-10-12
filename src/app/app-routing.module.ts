ced1cfb86d2c5160486adb6cf63feba005fe4f6e
Novke <novke.brate@gmail.com>
2024-10-12 20:49:09 +0200

    ciscenje pocetne & rutiranje

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartPredavanjeComponent } from './predavanje/start-predavanje/start-predavanje.component';
+import { LivePredavanjeComponent } from './predavanje/live-predavanje/live-predavanje.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"predavanje/start", component: StartPredavanjeComponent},
+  {path:"predavanje/live", component: LivePredavanjeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
