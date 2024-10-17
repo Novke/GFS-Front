4e9ef5ac28e0524345fa4dea2c3183c9bc22894e
Novke <novke.brate@gmail.com>
2024-10-17 09:47:39 +0200

    domaci fetchuje predmete i grupe

--- src/app/domaci/domaci.service.ts
+++ src/app/domaci/domaci.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { DodajDomaciCmd, DomaciInfo } from '../models/model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DomaciService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  dodajDomaci(cmd: DodajDomaciCmd){
+    return this.http.post<DomaciInfo>(`${this.apiUrl}/domaci`, cmd)
+  }
+
 }
--- src/app/domaci/nov-domaci/nov-domaci.component.ts
+++ src/app/domaci/nov-domaci/nov-domaci.component.ts
@@ -1,25 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { DodajDomaciCmd, GrupaInfo, PredavanjeInfo, PredmetInfo } from 'src/app/models/model';
 import { DomaciService } from '../domaci.service';
+import { PredavanjeService } from 'src/app/predavanje/predavanje.service';
 
 @Component({
   selector: 'app-nov-domaci',
   templateUrl: './nov-domaci.component.html',
   styleUrls: ['./nov-domaci.component.css']
 })
-export class NovDomaciComponent {
+export class NovDomaciComponent implements OnInit{
 
   grupe : GrupaInfo[] = []
   predmeti: PredmetInfo[] = []
   predavanja: PredavanjeInfo[] = []
   izabranaGrupa : number = 0
   izabranPredmet : number = 0
-  izabranoPredavanje : number = 0
+  izabranoPredavanje : number | undefined
 
-  constructor(private domaciService: DomaciService){
+  constructor(
+    private domaciService: DomaciService,
+    private predavanjaService: PredavanjeService){
+  }
+
+  ngOnInit(): void {
+    this.predavanjaService.getGrupe().subscribe(
+      result => this.grupe = result
+    )
+    this.predavanjaService.getPredmeti().subscribe(
+      result => this.predmeti = result
+    )
   }
 
   dodajDomaci(){
+    const cmd : DodajDomaciCmd = {
+      grupaId: this.izabranaGrupa,
+      predmetId: this.izabranPredmet,
+      predavanjeId: this.izabranoPredavanje
+    }
+
+    this.domaciService.dodajDomaci(cmd).subscribe(
+      result => console.log("ID =", result.id)
+    )
   }
 
 }
