02e05ae760272b08ebe9787005d4b76c2d916dda
Novke <novke.brate@gmail.com>
2024-11-18 16:09:48 +0100

    test select

--- src/app/test-select/test-select.component.ts
+++ src/app/test-select/test-select.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppRoutes } from '../app.routes';
 
 @Component({
   selector: 'app-test-select',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./test-select.component.css']
 })
 export class TestSelectComponent {
+  izabranaGrupa: number = 0;
+  izabranPredmet: number = 0;
+
+  constructor(private router: Router){}
+
+  onSubmit(){
+    if (this.izabranaGrupa && this.izabranPredmet) {
+    }
+  }
+
+  onNew(){
+    this.router.navigate([AppRoutes.testNew])
+  }
 
 }
