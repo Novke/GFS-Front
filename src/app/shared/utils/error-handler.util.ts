1d1c72a88ee6d8cedd6448e4c3026bbc593df47c
Novke <novke.brate@gmail.com>
2024-11-21 21:50:58 +0100

    error handler class

---
+++ src/app/shared/utils/error-handler.util.ts
@@ -0,0 +1,12 @@
+import { HttpErrorResponse } from '@angular/common/http';
+
+export class ErrorHandlerUtil {
+  static handleHttpError(error: HttpErrorResponse): void {
+    if (error.status >= 400 && error.status < 500) {
+      const reason = error.error?.reason || 'Sistemska greska'; // Safe navigation
+      alert(`Greska: ${reason}`);
+    } else {
+      console.error('Neuspesno kreiranje testa', error);
+    }
+  }
+}
