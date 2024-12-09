8b3cee100bf1cb9583fc55745b798d70e07e9672
Novke <novke.brate@gmail.com>
2024-12-09 17:59:13 +0100

    osnovni detalji o testu

--- src/app/test/test-pregled/test-pregled.component.ts
+++ src/app/test/test-pregled/test-pregled.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TestDetails } from 'src/app/models/model';
+import { TestService } from '../test.service';
+import { AppRoutes } from 'src/app/app.routes';
 
 @Component({
   selector: 'app-test-pregled',
   templateUrl: './test-pregled.component.html',
   styleUrls: ['./test-pregled.component.css']
 })
-export class TestPregledComponent {
+export class TestPregledComponent implements OnInit{
+
+  id: number | null = null
+  test: TestDetails | undefined
+
+  constructor(
+    private testService: TestService,
+    private route: ActivatedRoute,
+    private router: Router
+  ){}
+
+  ngOnInit(){
+    this.route.paramMap.subscribe(
+      params => {
+        const id = params.get('id')
+        this.id = id !== null ? Number(id) : null
+
+        if (this.id) this.ucitajTest()
+      }
+    )
+  }
+
+  private ucitajTest(){
+    this.testService.viewTest(Number(this.id)).subscribe(
+      result => {
+        this.popuniPolja(result)
+      }
+    )
+  }
+
+  private popuniPolja(result: TestDetails){
+    this.test = result
+  }
+
+  grupe2string(): string {
+    var out = "";
+    this.test?.grupe.sort().forEach( g => {
+      out+=g + " "
+    })
+    return out;
+  }
+
+  navigateEvidentiranje(){
+    if (this.id) this.router.navigate([AppRoutes.testEvidentiranje(Number(this.id))])
+  }
 
 }
