76439ced929dc6512f03abd35ecb1aecc24b0d27
Novke <novke.brate@gmail.com>
2024-12-09 20:55:11 +0100

    Mat table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FlexModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StartPredavanjeComponent } from './predavanje/start-predavanje/start-predavanje.component';
-import { LivePredavanjeComponent } from './predavanje/live-predavanje/live-predavanje.component';
+import { SelectBaseComponent } from './components/select-base.component';
+import { DomaciListComponent } from './domaci/domaci-list/domaci-list.component';
+import { DomaciSelectComponent } from './domaci/domaci-select/domaci-select.component';
 import { EvidentiranjeComponent } from './domaci/evidentiranje/evidentiranje.component';
 import { NovDomaciComponent } from './domaci/nov-domaci/nov-domaci.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatButtonModule } from '@angular/material/button'
-import { MatCardModule } from '@angular/material/card'
 import { PregledDomacegComponent } from './domaci/pregled-domaceg/pregled-domaceg.component';
-import { FlexModule } from '@angular/flex-layout'
-import { DomaciListComponent } from './domaci/domaci-list/domaci-list.component';
-import { MatListModule } from '@angular/material/list';
-import { DomaciSelectComponent } from './domaci/domaci-select/domaci-select.component';
-import { SelectBaseComponent } from './components/select-base.component';
+import { HomeComponent } from './home/home.component';
+import { LivePredavanjeComponent } from './predavanje/live-predavanje/live-predavanje.component';
 import { PredavanjeListComponent } from './predavanje/predavanje-list/predavanje-list.component';
 import { PredavanjeSelectComponent } from './predavanje/predavanje-select/predavanje-select.component';
 import { PregledPredavanjaComponent } from './predavanje/pregled-predavanja/pregled-predavanja.component';
-import { HomeComponent } from './home/home.component';
+import { StartPredavanjeComponent } from './predavanje/start-predavanje/start-predavanje.component';
 import { NovTestComponent } from './test/nov-test/nov-test.component';
-import { TestSelectComponent } from './test/test-select/test-select.component';
-import { TestListComponent } from './test/test-list/test-list.component';
 import { TestEvidentiranjeComponent } from './test/test-evidentiranje/test-evidentiranje.component';
+import { TestListComponent } from './test/test-list/test-list.component';
 import { TestPregledComponent } from './test/test-pregled/test-pregled.component';
+import { TestSelectComponent } from './test/test-select/test-select.component';
 
 @NgModule({
   declarations: [
@@ -60,7 +63,10 @@ import { TestPregledComponent } from './test/test-pregled/test-pregled.component
     MatButtonModule,
     MatCardModule,
     FlexModule,
-    MatListModule
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/test/test-pregled/test-pregled.component.ts
+++ src/app/test/test-pregled/test-pregled.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
-import { TestDetails } from 'src/app/models/model';
-import { TestService } from '../test.service';
 import { AppRoutes } from 'src/app/app.routes';
+import { StudentInfo, TestDetails, TestPolaganjeInfo } from 'src/app/models/model';
+import { TestService } from '../test.service';
 
 @Component({
   selector: 'app-test-pregled',
@@ -10,9 +13,14 @@ import { AppRoutes } from 'src/app/app.routes';
   styleUrls: ['./test-pregled.component.css']
 })
 export class TestPregledComponent implements OnInit{
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   id: number | null = null
   test: TestDetails | undefined
+
+  kolone: string[] = ['name', 'index', 'group', 'points'];
+  dataSource = new MatTableDataSource<TestPolaganjeInfo>();
 
   constructor(
     private testService: TestService,
@@ -41,6 +49,9 @@ export class TestPregledComponent implements OnInit{
 
   private popuniPolja(result: TestDetails){
     this.test = result
+    this.dataSource.data = result.polaganja
+    this.dataSource.paginator = this.paginator
+    this.dataSource.sort = this.sort
   }
 
   grupe2string(): string {
@@ -49,6 +60,14 @@ export class TestPregledComponent implements OnInit{
       out+=g + " "
     })
     return out;
+  }
+
+  student2string(student: StudentInfo){
+    return student.ime + " " + student.prezime + " " + student.indeks
+  }
+
+  grupa2string(polaganje: TestPolaganjeInfo){
+
   }
 
   navigateEvidentiranje(){
