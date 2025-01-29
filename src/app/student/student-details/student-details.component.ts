4dd4a546408eed07babc4ede45c9db659c480f51
Novke <novke.brate@gmail.com>
2025-01-29 12:38:48 +0100

    Student Details basic info

--- src/app/student/student-details/student-details.component.ts
+++ src/app/student/student-details/student-details.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { StudentPregledDetails } from 'src/app/models/model';
+import { StudentService } from '../student.service';
 
 @Component({
   selector: 'app-student-details',
   templateUrl: './student-details.component.html',
   styleUrls: ['./student-details.component.css']
 })
-export class StudentDetailsComponent {
+export class StudentDetailsComponent implements OnInit{
+
+  id: number | null = null
+  student: StudentPregledDetails | undefined
+
+  constructor(
+    private studentService: StudentService,
+        private route: ActivatedRoute,
+        private router: Router
+  ){}
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      params => {
+        const id = params.get('id')
+        this.id = id !== null ? Number(id) : null
+
+        if (this.id) this.ucitajStudenta()
+      }
+    )
+  }
+
+  private ucitajStudenta(){
+    this.studentService.getDetails(Number(this.id)).subscribe(
+      result => {
+        this.popuniPolja(result)
+      }
+    )
+  }
+
+  private popuniPolja(details: StudentPregledDetails){
+      this.student = details
+  }
+
+  navigateBack(){
+
+  }
 
 }
